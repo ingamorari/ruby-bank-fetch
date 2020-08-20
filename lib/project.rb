4109a66093b8d350441281703b1755aef473551a
nedeleadamian <69190192+nedeleadamian@users.noreply.github.com>
2020-08-20 12:38:52 +0300

    Revert "Dev (#2)"
    This reverts commit 8f6fd6fb53654b6d04c8e993c2ee5e56b99148d7.

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,13 @@
-require_relative "./project/fetch/fetch"
+# frozen_string_literal: true
+
+require "project/version"
+require "constants/constants"
+require 'watir'
+require 'selenium-webdriver'
 module Project
   class Error < StandardError;
   end
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(:chromeOptions => {detach: true })
+  b = Watir::Browser.new(:chrome, desired_capabilities: caps)
+  b.goto("https://facebook.com")
 end
